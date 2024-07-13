530055c1fa61125f54a0ebaee4ae3e4465610be9
toast <toastts@icloud.com>
2024-07-12 18:56:12 -0700

    added colors to tailwind config, need to migrate to them

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,20 +19,61 @@ const config = {
     },
     extend: {
       colors: {
-        // ui component bg
+        // Light Mode Colors
+        light: {
+          background: {
+            primary: '#F8FAF8',
+            secondary: '#E7E9E7',
+            tertiary: '#DFE2DF',
+            input: '#EFF1EF',
+            blur: '#F8FAF8',
+          },
+          text: {
+            primary: '#1D211C',
+            secondary: '#60655F',
+            tertiary: '#7F847D',
+            placeholder: '#898E87',
+            disabled: '#B9BCB8',
+            button: '#05F500',
+          },
+          border: {
+            primary: '#B9BCB8',
+            secondary: '#CCCFC6',
+            tertiary: '#DFE2DF',
+          },
+        },
+        // Dark Mode Colors
+        dark: {
+          background: {
+            primary: '#181917',
+            secondary: '#282A27',
+            tertiary: '#2F312E',
+            input: '#212220',
+            blur: '#181917',
+          },
+          text: {
+            primary: '#ECEEEC',
+            secondary: '#AFB5AD',
+            tertiary: '#767D74',
+            placeholder: '#687066',
+            disabled: '#5C625B',
+            button: '#05FF00',
+          },
+          border: {
+            primary: '#5C625B',
+            secondary: '#454843',
+            tertiary: '#2F312E',
+          },
+        },
+        // UI component background (example of old config)
         'background-secondary': '#282A27',
-
-        // ui borders
+        // UI borders
         'background-border': '#2F312E',
-
-        // primary text color
+        // Primary text color
         'text-primary': '#AFB5AD',
-
-
-        // app main background
+        // App main background
         'background-primary': '#181917',
-
-        // bright branding color
+        // Bright branding color
         'branding-bright': '#05FF00',
       },
       keyframes: {
